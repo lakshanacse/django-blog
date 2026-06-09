8467ca24672b14e5946c97152221d727687bd190
Lakshana Gopalakrishnan <12734@Tessolveindia.com>
2026-06-09 14:24:15 +0530

    registration and login

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATICFILES_DIRS=[
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
+
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,9 +25,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('blogs.urls')),
-    path('<slug:slug>/',BlogsView.blogs,name='blogs'),
+    path('blogs/<slug:slug>/',BlogsView.blogs,name='blogs'),
     # search endpoint
     path('blogs/search/',BlogsView.search,name="search"),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
 ] 
 
 
