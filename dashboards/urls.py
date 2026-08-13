bcd0fad7be7d2fe129688aaa294dd0bd2bdabaaf
Lakshana Gopalakrishnan <12734@Tessolveindia.com>
2026-08-13 15:22:59 +0530

    post crud operations/features

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -6,8 +6,14 @@ urlpatterns = [
    path('categories/',views.categories,name='categories'),
    path('posts/',views.posts,name='posts'),
 
+# Category CRUD
    path('categories/add/',views.add_category,name='add_category'),
    path('categories/edit/<int:category_id>/',views.edit_category,name='edit_category'),
    path('categories/delete/<int:category_id>/',views.delete_category,name='delete_category'),
+
+#Post CRUD
+ path('posts/add/',views.add_post,name='add_post'),
+ path('posts/edit/<int:post_id>',views.edit_post,name='edit_post'),
+  path('posts/delete/<int:post_id>',views.delete_post,name='delete_post'),
 
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
 
-from dashboards.forms import CategoryForm
-
+from dashboards.forms import BlogPostForm, CategoryForm
+from django.template.defaultfilters import slugify
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
@@ -19,7 +19,11 @@ def categories(request):
     return render(request,'dashboard/categories.html')
 
 def posts(request):
-    return render(request,'dashboard/posts.html')
+    posts=Blog.objects.all()
+    context={
+        'posts':posts,
+    }
+    return render(request,'dashboard/posts.html',context)
 
 def add_category(request):
     if request.method=='POST':
@@ -51,3 +55,47 @@ def delete_category(request,category_id):
     category=get_object_or_404(Category,pk=category_id)
     category.delete()
     return redirect('categories')
+
+def add_post(request):
+    if request.method=='POST':
+        form=BlogPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False) # temporarily saving the form
+            post.author=request.user
+            post.save()
+            title=form.cleaned_data['title']
+            post.slug=slugify(title) + '-' + str(post.id)
+            post.save()
+            return redirect('posts')
+        else:
+            print(form.errors)
+    
+    form=BlogPostForm()
+    context={
+        'form':form,
+    }
+    return render(request,'dashboard/add_post.html',context)
+
+def edit_post(request,post_id):
+    post=get_object_or_404(Blog,pk=post_id)
+    if request.method=='POST':
+        form=BlogPostForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            post=form.save()
+            title=form.cleaned_data['title']
+            post.slug=slugify(title) + '-' + str(post.id)
+            post.save()
+            return redirect('posts')
+    form=BlogPostForm(instance=post)
+    context={
+        'form':form,
+        'post':post
+
+    }
+    return render(request,'dashboard/edit_post.html',context)
+
+def delete_post(request,post_id):
+    post=get_object_or_404(Blog,pk=post_id)
+    post.delete()
+    return redirect('posts')
+
