81fc7018c4ad0221192687670c28b18e68129a98
Lakshana Gopalakrishnan <12734@Tessolveindia.com>
2026-08-20 15:10:35 +0530

    Comment feature

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About, Category,Blog, SocialLink
+from .models import About, Category,Blog, SocialLink, Comment
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
@@ -20,3 +20,4 @@ admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
 admin.site.register(About,AboutAdmin)
 admin.site.register(SocialLink)
+admin.site.register(Comment)
